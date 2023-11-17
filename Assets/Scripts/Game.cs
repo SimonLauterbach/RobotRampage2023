07ada9ca1dbf9bcdbe0e63d3a841f1249ada4fe8
SimonLauterbach <simon.lauterbach02@gmail.com>
2023-11-17 09:02:05 -0500

    Chapter 11 - Added Game Over UI

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
 
 
 public class Game : MonoBehaviour
 {
+    public GameObject gameOverPanel;
     public GameUI gameUI;
     public GameObject player;
     public int score;
@@ -76,6 +79,35 @@ public class Game : MonoBehaviour
             score += 1;
             gameUI.SetScoreText(score);
         }
+    }
+    public void OnGUI()
+    {
+        if (isGameOver && Cursor.visible == false)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    public void GameOver()
+    {
+        isGameOver = true;
+        Time.timeScale = 0;
+        player.GetComponent<FirstPersonController>().enabled = false;
+        player.GetComponent<CharacterController>().enabled = false;
+        gameOverPanel.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(Constants.SceneBattle);
+        gameOverPanel.SetActive(true);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(Constants.SceneMenu);
     }
 
 }
